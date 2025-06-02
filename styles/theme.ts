d8431a81d04c071d1418e5b61bf278dab25bd613
KatekaniN <knyamandi99@gmail.com>
2025-06-02 13:17:01 +0200

    test

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,8 +2,8 @@ export const COLORS = {
   primary: "#FF8FAB", // Medium pink
   secondary: "#FFB3C6", // Light pink
   background: "#121212", // Dark background
-  surface: "#1a1a1a", // Already defined
-  surfaceLight: "#2a2a2a", // Already defined
-  white: "#fff", // Already defined
+  surface: "#000", // light black
+  surfaceLight: "#2a2a2a", // Dark grey
+  white: "#fff", // White
   grey: "#9E9E9E", // Medium grey
 };
